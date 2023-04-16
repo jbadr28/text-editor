4fe84b5cdd8bd90d6f971b61fc6c59b6cb2346f2
jbadr28 <badreddine.jalili@etu.uae.ac.ma>
2023-04-16 16:52:23 +0000

    probability_dict added

--- Model/edit.py
+++ Model/edit.py
@@ -1,4 +1,34 @@
 import pickle
-with open('word_prob.pkl','rb') as f:
+import tkinter as tk
+import re
+import UI
+from UI.main import NotepadUI
+
+with open('probabilty.pkl','rb') as f:
     probs = pickle.load(f)
-print(probs)
+
+
+def delete_letter(word, verbose=False):
+    '''
+    Input:
+        word: the string/word for which you will generate all possible words
+                in the vocabulary which have 1 missing character
+    Output:
+        delete_l: a list of all possible strings obtained by deleting 1 character from word
+    '''
+
+    delete_l = []
+    split_l = []
+
+    ### START CODE HERE ###
+    for c in range(len(word)):
+        split_l.append((word[:c], word[c:]))
+    for a, b in split_l:
+        delete_l.append(a + b[1:])
+    ### END CODE HERE ###
+
+    if verbose: print(f"input word {word}, \nsplit_l = {split_l}, \ndelete_l = {delete_l}")
+
+    return delete_l
+
+
